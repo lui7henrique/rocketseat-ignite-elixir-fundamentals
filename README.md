fa477ed31cbeb52c2c55a1612199189facee2ef2
Luiz Henrique <7henrique18@gmail.com>
2021-11-28 03:56:46 -0300

    :sparkles: Enum module

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ iex.bat -S mix
 ## To study:
 
 - [**Recursividade e Tail Call Optimization**](https://www.notion.so/Recursividade-e-Tail-Call-Optimization-79f2a8103b174d6db58d8bea19546c0d)
+- [**Enum**](https://hexdocs.pm/elixir/1.12/Enum.html)
+- [**Functions (Docs)**](https://elixirschool.com/en/lessons/basics/functions/)
--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,4 +1,21 @@
 defmodule SumList do
+  def call_enum_sum(list), do: Enum.sum(list)
+  def call_enum_max(list), do: Enum.max(list)
+  def call_enum_min(list), do: Enum.min(list)
+
+  # lambda function using map to calc double of elements in list
+  def call_enum_map(list), do: Enum.map(list, fn elem -> elem * 2 end)
+
+  # lambda function using filter to return just element biggest 100
+  def call_enum_filter(list), do: Enum.filter(list, fn elem -> elem > 100 end)
+
+  # lambda function using any to return if any of the elements satisfies the condition
+  def call_enum_any(list), do: Enum.any?(list, fn elem -> elem > 100 end)
+
+  # lambda function using any to return if any of the elements satisfies the condition
+  def call_enum_all(list), do: Enum.all?(list, fn elem -> elem > 5 end)
+
+  # recursive call
   def call(list), do: sum(list, 0)
 
   defp sum([], acc), do: acc
